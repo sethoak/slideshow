5086601545eba1063a77856d6882f02141186a0e
sethoak <sethoak@gmail.com>
2019-09-08 18:27:22 -0500

    can't get it to go, but posting work so far

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 console.log("Hello World");
 
+/*
 document.getElementById("slides").innerHTML =
   "<ol><li>html data</li><li>Slide 1</li></ol>";
+  */
+
+var slides = document.querySelectorAll("#slides .slide");
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide, 2000);
+
+function nextSlide() {
+  slides[currentSlide].className = "slide";
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].className = "slide showing";
+}
